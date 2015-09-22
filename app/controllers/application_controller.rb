245064147fe25d184a96c18615f5a3c9e4062aa1
Matt Ewell <matt.ewell@gmail.com>
2015-09-22 12:48:27 -0600

    Add total functionality in cart view for demo Currently using a helper in application controller. This will be moved to cart model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :dollar_string, :cart
+  helper_method :dollar_string, :cart, :cart_total
 
   def cart
     Cart.new(session[:cart]).items || []
+  end
+
+  #move to cart model
+  def cart_total(cart_items)
+    cart_items.reduce(0) { |acc, item| acc + item.price }
   end
 
   def dollar_string(cents)
