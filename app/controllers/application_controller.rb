8bc104adba0a256f3d8f853b21414789eb2e107e
Adam Ki Jensen <adajensen@gmail.com>
2015-09-21 20:10:50 -0600

    feature testing for items being displayed on items view page. tests passing.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :dollar_string
+
+  def dollar_string(cents)
+    "$#{cents.to_s}".insert(-3, '.')
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+items = Item.create([{title: 'pencil', description: 'bulk pencils', price: 1020, image: 'https://pencil-outlet.com'},
+                     {title: 'haircut', description: 'the most stylish', price: 1599, image: 'https://haircuts.com'}])
