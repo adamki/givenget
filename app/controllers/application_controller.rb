8533bbed87d7ca5f1a781689027eb3446ea2a034
Matt Ewell <matt.ewell@gmail.com>
2015-09-22 12:12:20 -0600

    Add helper_method tag for cart helper in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :dollar_string
+  helper_method :dollar_string, :cart
 
   def cart
     Cart.new(session[:cart]).items || []
   end
-  
+
   def dollar_string(cents)
     "$#{cents.to_s}".insert(-3, '.')
   end
