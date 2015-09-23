bd2e1f37add2ce3be0371b2701a10aa21365aab6
Adam Ki Jensen <adajensen@gmail.com>
2015-09-23 09:07:26 -0600

    seed data added for categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,6 +22,7 @@ items = Item.create([{title: 'pencil', description: 'bulk pencils', price: 1020,
                      {title: 'broken fork', description: 'its only kinda broken', price: 2000, image: 'https://broken-fork.com'},
                      {title: 'crate', description: 'put some stuff in them', price: 1279, image: 'https://crates.com'}])
 
+categories = Category.create([{name: "Profit"}, {name: "Business"}, {name: "Education"}, {name: "Technology"}, {name: "Sporting goods"}, {name: "Travel"}, {name: "Home Furnishings"}, {name: "Small Business"}])
 
 
 
