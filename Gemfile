471bb458076a5068b80d7c38f469db9fdb0ef1c2
George Hudson <georgehudson78@gmail.com>
2015-09-23 12:15:21 -0600

    added omniauth tests for auth/twitter testing

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'puma'
 gem 'pg'
+gem 'omniauth'
 gem 'omniauth-twitter'
 
 group :development, :test do
--- spec/features/unauthenticated_user_can_login_spec.rb
+++ spec/features/unauthenticated_user_can_login_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 feature 'user interacts with the login' do
-  context 'unauthenticated with no account' do
+  context 'unauthenticated with or without account' do
 
-    xit 'can create an account' do
+    it 'can create an account or login if account already made' do
       visit '/'
-      click_on 'Create Account'
+      click_on 'Login'
+      #do omniauth twitter stuff
+      expect(current_path).to eq('/dashboard')
+      within('.navbar') do
+        expect(page).to have_content('Logged in as Test User') #my user name
+        expect(page).to have_link('Logout')
+        expect(page).to_not have_link('Login')
+      end
+      expect(page).to have_content('') #profile info
     end
 
   end
