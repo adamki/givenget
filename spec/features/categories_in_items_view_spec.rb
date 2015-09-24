5d915684c003cc8cfabc2a1f99b25759230fa19c
Matt Ewell <matt.ewell@gmail.com>
2015-09-23 21:51:01 -0600

    Add category list to individual item view Links not yet active

--- spec/features/categories_in_items_view_spec.rb
+++ spec/features/categories_in_items_view_spec.rb
@@ -8,4 +8,13 @@ feature 'categories in items view' do
       expect(page).to have_content(cat.title)
     end
   end
+
+  it "displays an item's categories in individual view" do
+    item = Item.first
+    visit item_path(item)
+    expect(current_path).to eq(item_path(item))
+    item.categories.each do |cat|
+      expect(page).to have_content(cat.title)
+    end
+  end
 end
