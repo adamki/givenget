9c4c21f4d38dc81b3c4464f1361d722d4c54692e
George Hudson <georgehudson78@gmail.com>
2015-09-23 18:35:54 -0600

    styling for front page WIP

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20150923013228) do
-=======
 ActiveRecord::Schema.define(version: 20150923031347) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
