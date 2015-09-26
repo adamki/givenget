dcad9535abd99801b2fe4c77993bbcc3b7585652
George Hudson <georgehudson78@gmail.com>
2015-09-26 17:52:15 -0600

    test for user can create account/login with twitter test updated for new functionality

--- spec/features/unauthenticated_user_can_login_spec.rb
+++ spec/features/unauthenticated_user_can_login_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-feature 'user interacts with the login' do
-  context 'unauthenticated with or without account' do
+feature 'unauthenticated user create account and login' do
+  context 'with valid account from twitter' do
 
-    it 'can create an account or login if account already made' do
+    it 'sees the dashboard' do
       visit '/'
-      click_on 'Login'
-      #do omniauth twitter stuff
+      click_on 'Login/Create Account'
+      click_on 'twitter-link' #do omniauth twitter stuff
       expect(current_path).to eq('/dashboard')
       within('.navbar') do
         expect(page).to have_content('Logged in as Test User') #my user name
@@ -16,8 +16,15 @@ feature 'user interacts with the login' do
       expect(page).to have_content('Welcome Test User') #profile info
       expect(page).to have_content('Donate Items')
       expect(page).to have_content('View Items')
-      expect(page).to have_content("Your Give'n'Get history:")
+      expect(page).to have_content('Your Give'n'Get history:')
     end
 
   end
+
+  context 'with valid attributes' do
+    visit '/'
+    click_on 'Login/Create Account'
+    click_on 'Create an account'
+  end
+
 end
