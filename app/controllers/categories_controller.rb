017509a0bad8108bd9eb02ec779b2a7786cefd9f
Matt Ewell <matt.ewell@gmail.com>
2015-09-25 20:41:15 -0600

    Continue work on categories crud Also add link to each category from items index, change item index page to have heading categries above category list instead of givengit. Fix seed data to work with category validation in items

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,15 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def show
+    @category = Category.find(params[:id])
+    # need action for non-existent category
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.delete
+    redirect_to categories_path
+  end
 end
