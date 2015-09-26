514e0298798eb07b0be17f045670df6e8a60f2eb
George Hudson <georgehudson78@gmail.com>
2015-09-26 12:01:02 -0600

    fleshed out the item tests to use as a better example for user and identity tests

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,7 +5,7 @@ describe 'item' do
 
   let(:attributes){{title: 'pencil', description: 'bulk pencils', price: 1020, image: 'https://pencil-outlet.com'}}
 
-    it 'has attributes' do
+    it 'is valid with valid attributes' do
       item = Item.new(attributes)
       expect(item.title).to eq('pencil')
       expect(item.description).to eq('bulk pencils')
@@ -16,5 +16,30 @@ describe 'item' do
 
   end
 
-  
+  context 'with invalid attributes' do
+
+    let(:attributes){{title: 'pencil', description: 'bulk pencils', price: 1020, image: 'https://pencil-outlet.com'}}
+
+    it 'requires title attributes' do
+      item = Item.new(attributes)
+      item.title = nil
+
+      expect(item).to_not be_valid
+    end
+
+    it 'requires description attributes' do
+      item = Item.new(attributes)
+      item.description = nil
+
+      expect(item).to_not be_valid
+    end
+
+    it 'requires price attributes' do
+      item = Item.new(attributes)
+      item.price = nil
+
+      expect(item).to_not be_valid
+    end
+  end
+
 end
