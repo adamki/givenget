57f9d72e5db0555b4de27dfff20072a793207f03
Jerrod Junker <unsafepond@gmail.com>
2015-09-28 11:40:29 -0600

    changed logic in carts controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,8 +5,12 @@ class CartsController < ApplicationController
 
   def create
   	item_id = params[:cart][:item]
-  	cart_items_session[item_id] = params[:cart][:quantity] if params[:cart][:quantity] != ""
-    flash.notice = "Item added to cart!"
+    if params[:cart][:quantity].to_i > 0
+      cart_items_session[item_id] = params[:cart][:quantity]
+      flash.notice = "Item added to cart!" 
+    else
+      flash.notice = "Inavalid Quantity! :( " 
+    end
     cart
   	redirect_to '/items'
   end
