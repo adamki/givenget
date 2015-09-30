cfb79890bf6cc885d40963fc197a0e3e08dd6051
Matt Ewell <matt.ewell@gmail.com>
2015-09-30 12:02:53 -0600

    Factor before methods to spec helper

--- spec/features/cart_cleared_after_checkout_spec.rb
+++ spec/features/cart_cleared_after_checkout_spec.rb
@@ -2,16 +2,11 @@ require 'rails_helper'
 
 feature 'Cart cleared after checkout' do
   before do
-    visit '/signin_or_signup'
-    click_link_or_button("twitter-link")
-    visit '/items'
-    click_link_or_button('pencil')
-    click_link_or_button("Add to Cart")
-    visit '/cart'
-    expect(page).to have_content("pencil")
+    fill_cart
   end
 
   it 'clears the cart' do
+    expect(page).to have_content("pencil")
     click_link_or_button("Checkout")
     visit '/cart'
     expect(page).to have_content('You have no items in your cart')
---
+++ spec/features/user_provides_address_on_checkout_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+feature 'checkout' do
+  before do
+    create_order
+  end
+  it 'requires a mailing address on checkout' do
+
+  end
+end
