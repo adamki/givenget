3a34e9d71ba202c2e28a8d708c0408b529ff705e
Matt Ewell <matt.ewell@gmail.com>
2015-10-01 00:37:53 -0600

    Complete admin orders functionality Can click on buttons to change order status There is a count of orders by status Clicking on the status count will show only the orders with that status (aka filtering)

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,35 @@
 class Admin::OrdersController < AdminController
 
   def index
+    @counts = Order.status_counts
     @orders = Order.all
   end
+
+  def paid
+    @orders = Order.where(status: "Paid")
+  end
+
+  def cancelled
+    @orders = Order.where(status: "Cancelled")
+  end
+
+  def ordered
+    @orders = Order.where(status: "Ordered")
+  end
+
+  def completed
+    @orders = Order.where(status: "Completed")
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def update
+    order = Order.find(params[:id])
+    order.change_status(params[:new_status])
+    redirect_to admin_orders_path
+  end
+
 
 end
