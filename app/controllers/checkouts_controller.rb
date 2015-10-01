9825f31a42bbb82cc022601ebad995c7ded801b3
Matt Ewell <matt.ewell@gmail.com>
2015-09-30 18:41:58 -0600

    Add addresses table Link one to one address and user. We're not going crazy with this at this hour. Checkouts controller updated to create address from the checkout 1 of 2 form. Next will be tackling assistance.

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,10 +1,26 @@
 class CheckoutsController < ApplicationController
   def new
-    
+    @address = Address.new
   end
 
+  def confirm
+  end
 
+  def create
+    @address = Address.new(address_params)
+    current_user.address = @address
+    @assistance = Assistance.new
+  end
 
-
+  private
+  def address_params
+    {full_name: params["full_name"],
+      street1: params["street1"],
+      street2: params["street2"],
+      city: params["city"],
+      state: params["state"],
+      zip: params["zip"]
+    }
+  end
 
 end
--- spec/features/user_provides_address_on_checkout_spec.rb
+++ spec/features/user_provides_address_on_checkout_spec.rb
@@ -1,18 +1,33 @@
 require 'rails_helper'
 
 feature 'checkout' do
+  let(:address) {
+    OpenStruct.new(full_name: "Abraham Lincoln",
+      add_1: "123 Main St.",
+      add_2: "Unit 7",
+      city: "Springfield",
+      state: "IL",
+      zip: "12345")
+  }
   before do
     fill_cart
   end
-  
-  it 'requires a mailing address on checkout' do
+
+  it 'takes a mailing address on checkout' do
     click_link_or_button('Checkout')
     expect(current_path).to eq('/checkouts/new')
-    fill_in 'Full Name', with: "Abraham Lincoln"
-    fill_in 'Address First Line', with: "123 Main St."
-    fill_in 'Address Second Line', with: "Unit 7"
-    fill_in 'City', with: "Springfield"
-    fill_in 'State', with: 'IL'
-    fill_in 'Zip', with: '12345'
+    fill_in 'Full Name', with: address.full_name
+    fill_in 'Address First Line', with: address.add_1
+    fill_in 'Address Second Line', with: address.add_2
+    fill_in 'City', with: address.city
+    fill_in 'State', with: address.state
+    fill_in 'Zip', with: address.state
+    expect(page).to have_content("You may request financial assistance on next screen")
+    click_button("Proceed to Payment")
+    expect(current_path).to eq('/checkouts/confirm')
   end
+
+  
+
+
 end
